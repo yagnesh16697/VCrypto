a9da527b1f7443f0cd646daee23acfcc9c5d23a4
yagnesh <yagnesh@itoneclick.com>
2021-03-21 11:29:51 +0530

    Navigation

---
+++ src/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,75 @@
+import { Entypo, Feather, AntDesign, Octicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import HomeScreen from '../screens/HomeScreen';
+import MarketScreen from '../screens/MarketScreen';
+import PortfolioScreen from '../screens/PortfolioScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import RankingsScreen from '../screens/RankingsScreen';
+import { BottomTabParamList } from '../../types';
+
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        style: { height: 60 },
+        activeTintColor: Colors[colorScheme].tint,
+      }}
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={30} color="black" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Portfolio"
+        component={PortfolioScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="pie-chart" size={30} color="black" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Market"
+        component={MarketScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="linechart" size={30} color="black" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Ranking"
+        component={RankingsScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons name="graph" size={30} color="black" />
+          ),
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="user" size={30} color="black" />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
---
+++ src/screens/RankingsScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RankingsScreen = (props) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ranking Screen</Text>
+    </View>
+  );
+};
+
+export default RankingsScreen;
